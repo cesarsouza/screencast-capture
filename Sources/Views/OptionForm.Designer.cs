b4c22443db2a6af7548d63b4cd355f14d4c88047
César Souza <cesarsouza@gmail.com>
2013-01-21 14:04:08 +0000

    Updating localization translation.

--- Sources/Views/OptionForm.Designer.cs
+++ Sources/Views/OptionForm.Designer.cs
@@ -34,6 +34,8 @@
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tbCopyright = new System.Windows.Forms.RichTextBox();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.cbConversion = new System.Windows.Forms.CheckBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -53,16 +55,14 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.btnApply = new System.Windows.Forms.Button();
             this.btnDonate = new System.Windows.Forms.Button();
-            this.cbConversion = new System.Windows.Forms.CheckBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.tabPage2.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.tabControl1.SuspendLayout();
-            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSave
@@ -82,21 +82,22 @@
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.tbCopyright);
             resources.ApplyResources(this.tabPage2, "tabPage2");
+            this.tabPage2.Controls.Add(this.tbCopyright);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tbCopyright
             // 
+            resources.ApplyResources(this.tbCopyright, "tbCopyright");
             this.tbCopyright.BackColor = System.Drawing.Color.White;
             this.tbCopyright.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            resources.ApplyResources(this.tbCopyright, "tbCopyright");
             this.tbCopyright.Name = "tbCopyright";
             this.tbCopyright.ReadOnly = true;
             // 
             // tabPage1
             // 
+            resources.ApplyResources(this.tabPage1, "tabPage1");
             this.tabPage1.Controls.Add(this.groupBox4);
             this.tabPage1.Controls.Add(this.groupBox3);
             this.tabPage1.Controls.Add(this.lbVersion);
@@ -105,17 +106,29 @@
             this.tabPage1.Controls.Add(this.pictureBox1);
             this.tabPage1.Controls.Add(this.groupBox1);
             this.tabPage1.Controls.Add(this.label1);
-            resources.ApplyResources(this.tabPage1, "tabPage1");
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // groupBox4
+            // 
+            resources.ApplyResources(this.groupBox4, "groupBox4");
+            this.groupBox4.Controls.Add(this.cbConversion);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.TabStop = false;
+            // 
+            // cbConversion
+            // 
+            resources.ApplyResources(this.cbConversion, "cbConversion");
+            this.cbConversion.Name = "cbConversion";
+            this.cbConversion.UseVisualStyleBackColor = true;
+            // 
             // groupBox3
             // 
+            resources.ApplyResources(this.groupBox3, "groupBox3");
             this.groupBox3.Controls.Add(this.label3);
             this.groupBox3.Controls.Add(this.label2);
             this.groupBox3.Controls.Add(this.cbContainer);
             this.groupBox3.Controls.Add(this.cbFrameRate);
-            resources.ApplyResources(this.groupBox3, "groupBox3");
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.TabStop = false;
             // 
@@ -131,13 +144,14 @@
             // 
             // cbContainer
             // 
+            resources.ApplyResources(this.cbContainer, "cbContainer");
             this.cbContainer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbContainer.FormattingEnabled = true;
-            resources.ApplyResources(this.cbContainer, "cbContainer");
             this.cbContainer.Name = "cbContainer";
             // 
             // cbFrameRate
             // 
+            resources.ApplyResources(this.cbFrameRate, "cbFrameRate");
             this.cbFrameRate.FormattingEnabled = true;
             this.cbFrameRate.Items.AddRange(new object[] {
             resources.GetString("cbFrameRate.Items"),
@@ -145,7 +159,6 @@
             resources.GetString("cbFrameRate.Items2"),
             resources.GetString("cbFrameRate.Items3"),
             resources.GetString("cbFrameRate.Items4")});
-            resources.ApplyResources(this.cbFrameRate, "cbFrameRate");
             this.cbFrameRate.Name = "cbFrameRate";
             // 
             // lbVersion
@@ -163,9 +176,9 @@
             // 
             // groupBox2
             // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
             this.groupBox2.Controls.Add(this.btnSelectFolder);
             this.groupBox2.Controls.Add(this.tbSavePath);
-            resources.ApplyResources(this.groupBox2, "groupBox2");
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.TabStop = false;
             // 
@@ -183,17 +196,17 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::ScreenCapture.Properties.Resources.screencast_128;
             resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Image = global::ScreenCapture.Properties.Resources.screencast_128;
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.TabStop = false;
             // 
             // groupBox1
             // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Controls.Add(this.cbMouseClicks);
             this.groupBox1.Controls.Add(this.cbKeyboard);
             this.groupBox1.Controls.Add(this.cbMouseCursor);
-            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.TabStop = false;
             // 
@@ -222,9 +235,9 @@
             // 
             // tabControl1
             // 
+            resources.ApplyResources(this.tabControl1, "tabControl1");
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            resources.ApplyResources(this.tabControl1, "tabControl1");
             this.tabControl1.Multiline = true;
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -238,26 +251,13 @@
             // 
             // btnDonate
             // 
-            this.btnDonate.BackColor = System.Drawing.Color.Transparent;
             resources.ApplyResources(this.btnDonate, "btnDonate");
+            this.btnDonate.BackColor = System.Drawing.Color.Transparent;
             this.btnDonate.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnDonate.FlatAppearance.BorderSize = 0;
             this.btnDonate.Name = "btnDonate";
             this.btnDonate.UseVisualStyleBackColor = false;
             this.btnDonate.Click += new System.EventHandler(this.btnDonate_Click);
-            // 
-            // cbConversion
-            // 
-            resources.ApplyResources(this.cbConversion, "cbConversion");
-            this.cbConversion.Name = "cbConversion";
-            this.cbConversion.UseVisualStyleBackColor = true;
-            // 
-            // groupBox4
-            // 
-            this.groupBox4.Controls.Add(this.cbConversion);
-            resources.ApplyResources(this.groupBox4, "groupBox4");
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.TabStop = false;
             // 
             // OptionForm
             // 
@@ -278,6 +278,8 @@
             this.tabPage2.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -286,8 +288,6 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.tabControl1.ResumeLayout(false);
-            this.groupBox4.ResumeLayout(false);
-            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
